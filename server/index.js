a63e77de147f95b35ee2fcb4197228116010a87d
vivian <vivian.v18723@gmail.com>
2022-07-25 11:27:24 +0100

    exported listener & port

--- server/index.js
+++ server/index.js
@@ -2,6 +2,11 @@ const app = require("./app");
 
 const port = 3000;
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+const listener = app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
 });
+
+module.exports = {
+  listener,
+  port,
+};
