2775a3f387219ebdf9bb0ecfbdcfb274bfa03aa4
vivian <vivian.v18723@gmail.com>
2022-07-25 19:30:34 +0100

    update data file

--- server/data.js
+++ server/data.js
@@ -1,14 +1,51 @@
-const posts = [
+const userPosts = [
   {
-    post: "text goes here", // original user comment (journal entry)
-    comments: ["text goes here", "text goes here", "text goes here"], // all responses to comment in post key
-    gifAPI: "link goes here", // image from api
-    emojis: {
-      love: 0,
-      like: 0,
-      dontlike: 0,
+    singleJournalEntry: "original single user entry goes here, 1", // original user journal entry
+    commentsSection: [
+      "comments for 1st entry",
+      "comments",
+      "comments",
+      "comments",
+    ], // all responses to entry, as many as you'd like
+    gifAPI: "api link goes here, 1", // image link for api
+    emojisCount: {
+      love: 1,
+      like: 1,
+      dontlike: 1,
+    },
+  },
+
+  {
+    singleJournalEntry: "original single user entry goes here, 2", // original user journal entry
+    commentsSection: [
+      "comments for 2nd entry",
+      "comments",
+      "comments",
+      "comments",
+    ], // all responses to entry, as many as you'd like
+    gifAPI: "api link goes here, 2", // image link for api
+    emojisCount: {
+      love: 2,
+      like: 2,
+      dontlike: 2,
+    },
+  },
+
+  {
+    singleJournalEntry: "original single user entry goes here, 3", // original user journal entry
+    commentsSection: [
+      "comments for 3rd entry",
+      "comments",
+      "comments",
+      "comments",
+    ], // all responses to entry, as many as you'd like
+    gifAPI: "api link goes here, 3", // image link for api
+    emojisCount: {
+      love: 3,
+      like: 3,
+      dontlike: 3,
     },
   },
 ];
 
-module.exports = posts;
+module.exports = userPosts;
