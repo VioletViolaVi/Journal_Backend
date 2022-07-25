5c0d1f5be86e9b73624a420629d41183819a3ec8
vivian <vivian.v18723@gmail.com>
2022-07-25 11:13:55 +0100

    add get() methods w/ endpoints

--- server/app.js
+++ server/app.js
@@ -10,23 +10,51 @@ app.get("/", (req, res) => {
   res.status(200).send("Hello World");
 });
 
-app.get("/students", (req, res) => {
+app.get("/posts", (req, res) => {
   res.status(200).send(data);
 });
 
-app.get("/students/:id", (req, res) => {
+app.get("/posts/post", (req, res) => {
+  res.status(200).send(data[0].post);
+});
+
+app.get("/posts/gifAPI", (req, res) => {
+  res.status(200).send(data[0].gifAPI);
+});
+
+app.get("/posts/comments/comment", (req, res) => {
+  res.status(200).send(data[0].comments[0].comment);
+});
+
+app.get("/posts/comments/love", (req, res) => {
+  res.status(200).send(data[0].comments[0].love.toString()); // needs .toString() to show
+});
+
+app.get("/posts/comments/like", (req, res) => {
+  res.status(200).send(data[0].comments[0].like.toString()); // needs .toString() to show
+});
+
+app.get("/posts/comments/dontlike", (req, res) => {
+  res.status(200).send(data[0].comments[0].dontlike.toString()); // needs .toString() to show
+});
+
+app.get("/posts/comments/response", (req, res) => {
+  res.status(200).send(data[0].comments[0].response);
+});
+
+app.get("/posts/:id", (req, res) => {
   const id = req.params.id;
 
   res.status(200).send(data[id - 1]);
 });
 
-app.post("/students", (req, res) => {
-  const newStudent = req.body;
-  data.push(newStudent);
+app.post("/posts", (req, res) => {
+  const newPost = req.body;
+  data.push(newPost);
 
   res.status(201).json({
     success: true,
-    student: newStudent,
+    posts: newPost,
   });
 });
 
