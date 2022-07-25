428307e4527b49e1bb9099f5848cab68c6ee182e
vivian <vivian.v18723@gmail.com>
2022-07-25 16:04:30 +0100

    add more tests

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -1,7 +1,17 @@
-const { port } = require("../index");
+const { portHost, listenFunc } = require("../index");
 
 describe("Testing Listener", () => {
-  test("Test to ensure port is 3000", () => {
-    expect(port).toEqual(3000);
+  test("Test to ensure port is 4000", () => {
+    expect(portHost()).toEqual(4000);
+  });
+
+  test("Test to ensure correct string is returned", () => {
+    expect(listenFunc()).toBe("Listening on port 4000...");
+  });
+});
+
+describe("Testing to check if is a function", () => {
+  test("Check if listener is in a function", () => {
+    expect(typeof listenFunc).toBe("function");
   });
 });
