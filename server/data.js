65ffbd62441dfeea377c0f57f7ad2dddc85f3e1a
vivian <vivian.v18723@gmail.com>
2022-07-25 18:13:18 +0100

    change data structure

--- server/data.js
+++ server/data.js
@@ -1,15 +1,13 @@
 const posts = [
   {
-    post: "text",
-    gifAPI: "link",
-    comments: [
-      {
-        comment: "text",
-        love: 0,
-        like: 0,
-        dontlike: 0,
-      },
-    ],
+    post: "text goes here", // original user comment (journal entry)
+    comments: ["text goes here", "text goes here", "text goes here"], // all responses to comment in post key
+    gifAPI: "link goes here", // image from api
+    emojis: {
+      love: 0,
+      like: 0,
+      dontlike: 0,
+    },
   },
 ];
 
