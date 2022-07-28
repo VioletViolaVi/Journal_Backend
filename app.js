6e8d45454305e0ab5e4b19e1a14452c73ef5d8e3
vivian <vivian.v18723@gmail.com>
2022-07-28 18:58:43 +0100

    create put requests for emojis

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const fs = require("fs");
-
+const fsPromises = require("fs").promises;
 const dataInJsonFile = JSON.parse(fs.readFileSync("data.json"), "utf-8");
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); // this is middleware needed for post request & to read req.body
 
 /*
 app.use(express.static(__dirname + "/../client"));
@@ -40,133 +40,91 @@ app.get("/userPosts/singleJournalEntry", (req, res) => {
   res.status(200).json(dataInJsonFile.userPostData[0].singleJournalEntry);
 });
 
+// the following routes make put requests using input entered by user in the journal blog website:
+
+// :loves
+app.put("/userPosts/updateLoves", (req, res) => {
+  let fromUser = req.body.emojisCount.love; // from user
+  console.log(fromUser);
+
+  async function updateLovesInFile(lovesParam) {
+    try {
+      // read file 1st
+      let dataFromJsonFile = await fsPromises.readFile("data.json");
+      // parse so it's readable
+      let obj = JSON.parse(dataFromJsonFile);
+
+      // set love property in the obj that you are trying to change
+      obj.userPostData[0].emojisCount.love = lovesParam;
+
+      // write file using what was read & the reassigned value
+      await fsPromises.writeFile("data.json", JSON.stringify(obj));
+    } catch (e) {
+      // error handling here
+      console.log(e);
+    }
+  }
+  updateLovesInFile(fromUser);
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// :likes
+app.put("/userPosts/updateLikes", (req, res) => {
+  let fromUser = req.body.emojisCount.like; // from user
+
+  async function updateLikesInFile(likesParam) {
+    try {
+      // read file 1st
+      let dataFromJsonFile = await fsPromises.readFile("data.json");
+      // parse so it's readable
+      let obj = JSON.parse(dataFromJsonFile);
+
+      // set like property in the obj that you are trying to change
+      obj.userPostData[0].emojisCount.like = likesParam;
+
+      // write file using what was read & the reassigned value
+      await fsPromises.writeFile("data.json", JSON.stringify(obj));
+    } catch (e) {
+      // error handling here
+      console.log(e);
+    }
+  }
+  updateLikesInFile(fromUser);
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+// :dislike
+app.put("/userPosts/updateDislike", (req, res) => {
+  let fromUser = req.body.emojisCount.dontlike; // from user
+
+  async function updateDislikesInFile(dislikesParam) {
+    try {
+      // read file 1st
+      let dataFromJsonFile = await fsPromises.readFile("data.json");
+      // parse so it's readable
+      let obj = JSON.parse(dataFromJsonFile);
+
+      // set like property in the obj that you are trying to change
+      obj.userPostData[0].emojisCount.dontlike = dislikesParam;
+
+      // write file using what was read & the reassigned value
+      await fsPromises.writeFile("data.json", JSON.stringify(obj));
+    } catch (e) {
+      // error handling here
+      console.log(e);
+    }
+  }
+  updateDislikesInFile(fromUser);
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
 module.exports = app;
-
-// const data = require("./data"); // aka => userPosts arr
-// const express = require("express");
-// const cors = require("cors");
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json()); // this is middleware needed for post request & to read req.body
-
-// /*
-// app.use(express.static(__dirname + "/../client"));
-// const path = require("path");
-// */
-
-// app.get("/", (req, res) => {
-//   res.status(200).send("User Journal Entries");
-//   /*
-//   res.sendFile(path.resolve("../client/index.html"))
-//   */
-// });
-
-// app.get("/userPosts", (req, res) => {
-//   res.status(200).send(data);
-// });
-
-// app.post("/userPosts", (req, res) => {
-//   const newUserPost = req.body;
-
-//   // newUserPost.id = data.length + 1; // for incrementing id num
-
-//   data.push(newUserPost);
-
-//   res.status(201).json({
-//     success: true,
-//     posts: newUserPost,
-//   });
-// });
-
-// app.get("/userPosts/:id", (req, res) => {
-//   const id = req.params.id;
-
-//   if (parseInt(id) <= 0 || parseInt(id) > data.length) {
-//     throw `there's no user post at id ${id}`;
-//   }
-
-//   res.status(200).send(data[id - 1].singleJournalEntry);
-
-//   // --- error handling for ids ---
-//   // const userPostToFind = data.filter((userPostObj) => {
-//   //   return userPostObj.id === parseInt(id); // id would be a string otherwise
-//   // });
-
-//   // // if not present return 404
-//   // if (!userPostToFind) {
-//   //   res.status(404).send("this user post could not be found");
-//   //   return;
-//   // }
-
-//   // // error handling
-//   // if (userPostToFind.length === 1) {
-//   //   res.status(200).send(userPostToFind[0]);
-//   //   return;
-//   // } else {
-//   //   res.status(404).send("there's no user post with that id");
-//   //   return;
-//   // }
-// });
-
-// app.get("/userPosts/:id/singleJournalEntry", (req, res) => {
-//   const id = req.params.id;
-
-//   if (parseInt(id) <= 0 || parseInt(id) > data.length) {
-//     throw `there's no single journal entry at id ${id}`;
-//   }
-
-//   res.status(200).send(data[id - 1].singleJournalEntry);
-// });
-
-// app.get("/userPosts/:id/gifAPI", (req, res) => {
-//   const id = req.params.id;
-
-//   if (parseInt(id) <= 0 || parseInt(id) > data.length) {
-//     throw `there's no gif at id ${id}`;
-//   }
-
-//   res.status(200).send(data[id - 1].gifAPI);
-// });
-
-// app.get("/userPosts/:id/commentsSection", (req, res) => {
-//   const id = req.params.id;
-
-//   if (parseInt(id) <= 0 || parseInt(id) > data.length) {
-//     throw `there's no comment section at id ${id}`;
-//   }
-
-//   res.status(200).send(data[id - 1].commentsSection);
-// });
-
-// app.get("/userPosts/:id/emojisCount/love", (req, res) => {
-//   const id = req.params.id;
-
-//   if (parseInt(id) <= 0 || parseInt(id) > data.length) {
-//     throw `there's no love emoji at id ${id}`;
-//   }
-
-//   res.status(200).send(data[id - 1].emojisCount.love.toString()); // needs .toString() to show
-// });
-
-// app.get("/userPosts/:id/emojisCount/like", (req, res) => {
-//   const id = req.params.id;
-
-//   if (parseInt(id) <= 0 || parseInt(id) > data.length) {
-//     throw `there's no like emoji at id ${id}`;
-//   }
-
-//   res.status(200).send(data[id - 1].emojisCount.like.toString()); // needs .toString() to show
-// });
-
-// app.get("/userPosts/:id/emojisCount/dontlike", (req, res) => {
-//   const id = req.params.id;
-
-//   if (parseInt(id) <= 0 || parseInt(id) > data.length) {
-//     throw `there's no don't like emoji at id ${id}`;
-//   }
-
-//   res.status(200).send(data[id - 1].emojisCount.dontlike.toString()); // needs .toString() to show
-// });
-
-// module.exports = app;
